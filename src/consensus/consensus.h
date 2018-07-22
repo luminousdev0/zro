b66cff30b4b12567e6d1278d4254cfc4374c82d8
luminousdev0 <38036255+luminousdev0@users.noreply.github.com>
2018-07-22 10:13:06 -0400

    init

--- src/consensus/consensus.h
+++ src/consensus/consensus.h
@@ -11,11 +11,11 @@
 #include <stdint.h>
 
 // HF constants
-static const int HF_LYRA2VAR_HEIGHT = 500;
-static const int HF_LYRA2_HEIGHT = 8192;
-static const int HF_LYRA2Z_HEIGHT = 20500;
-static const int HF_ZERONODE_HEIGHT = 66550;
-static const int HF_ZERONODE_PAYMENT_START = HF_ZERONODE_HEIGHT + 150; // 66700 - about 25h after HF
+static const int HF_LYRA2VAR_HEIGHT = 10;
+static const int HF_LYRA2_HEIGHT = 15;
+static const int HF_LYRA2Z_HEIGHT = 20;
+static const int HF_ZERONODE_HEIGHT = 100;
+static const int HF_ZERONODE_PAYMENT_START = HF_ZERONODE_HEIGHT + 50; // 66700 - about 25h after HF
 //static const int HF_MTP_HEIGHT = 70000;
 
 static const int HF_LYRA2VAR_HEIGHT_TESTNET = 10;
@@ -35,7 +35,7 @@ static const unsigned int MAX_BLOCK_BASE_SIZE = 2000000;
 /** The maximum allowed number of signature check operations in a block (network rule) */
 static const int64_t MAX_BLOCK_SIGOPS_COST = 400000;
 /** Coinbase transaction outputs can only be spent after this number of new blocks (network rule) */
-static const int COINBASE_MATURITY = 100;
+static const int COINBASE_MATURITY = 50;
 
 /** Flags for nSequence and nLockTime locks */
 enum {
